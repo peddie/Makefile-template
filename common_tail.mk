cbbf05b8046e56b4ffb22d580cecaf11321b1aa2
Matthew Peddie <peddie@alum.mit.edu>
2013-02-04 21:41:05 -0800

    Spice up the help message with some ifdefs, so it only talks about the relevant targets.

--- common_tail.mk
+++ common_tail.mk
@@ -1,32 +1,39 @@
 .PHONY: debug-% help
 
-# Usage message
+# Usage message.  The ifdef blocks turn things off if they're not
+# currently available based on our current best guess.
 help:
 	@echo 'Usage: '
 	@echo
 	@echo '    make              -- build the executable ($(PROJ))       '
-	@echo			     
+	@echo
 	@echo '    make Q=""         -- print full commands and their output:'
 	@echo '                         "gcc -Wall -c foo.c -o foo.o"        '
 	@echo '                         instead of "CC foo.o"                '
-	@echo			     
+	@echo
+ifdef SHAREDNAME
 	@echo '    make shared       -- build a shared library               '
 	@echo '                         ($(SHAREDNAME))'
-	@echo			     
+	@echo
+endif
+ifdef STATICNAME
 	@echo '    make static       -- build a static library               '
 	@echo '                         ($(STATICNAME))'
-	@echo			     
+	@echo
+endif
+ifdef TESTS_SRC
 	@echo '    make tests        -- compile the test suite               '
 	@echo '                         ($(TESTS))'
-	@echo			     
+	@echo
 	@echo '    make test         -- compile and run the test suite       '
-	@echo			     
+	@echo
 	@echo '    make foo_test_run -- compile and run the tests in         '
 	@echo '                         foo_test.c or foo_test.$(CXX_EXT)    '
-	@echo			     
-	@echo '    make clean        -- clean up the build		     '
-	@echo			     
-	@echo '    make debug-SRC    -- print everything that make 	     '
+	@echo
+endif
+	@echo '    make clean        -- clean up the build                   '
+	@echo
+	@echo '    make debug-SRC    -- print everything that make           '
 	@echo '                         knows about the variable $$(SRC)     '
 	@echo '                         (works for any variable name)        '
 	@echo
@@ -36,12 +43,14 @@ help:
 	@echo '    make lint         -- Run external linters (splint, cpplint.py,'
 	@echo '                         cppcheck) to find code problems      '
 	@echo
+ifdef AVRPROJ
 	@echo '    make avr-all      -- Build code for ATmega AVR chips      '
 	@echo
-	@echo '    make flash        -- program an AVR via an ISP            '
+	@echo '    make flash        -- program an AVR via an ISP'
 	@echo
-	@echo '    make flash-eeprom -- Upload an AVR EEPROM image via an ISP'
+	@echo '    make flash-eeprom -- Upload an AVR EEPROM image via an ISP '
 	@echo
+endif
 ifdef CM4_PROJ
 	@echo '    make cm4-all      -- Build code for ARM Cortex-M4 chips    '
 	@echo
@@ -60,6 +69,10 @@ ifneq "$(wildcard $(DOC_DIR) )" ""
 	@echo '    make doc(s)-clean -- clean up the project documentation.   '
 	@echo
 endif
+endif
+ifdef DEPS
+	@echo '    make deps         -- Install build dependencies.           '
+	@echo
 endif
 
 # Print debug information about any variable
